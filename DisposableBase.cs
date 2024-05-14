20d1765dae702d3b938c5a94d68ea0dadb9c71bf
Dmitry <bog22232@ya.ru>
2024-05-14 17:33:06 +0400

    Initialize

---
+++ DisposableBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Disposable
+{
+public abstract class DisposableBase : IDisposable
+{
+    protected bool disposed = false;
+
+    ~DisposableBase()
+    {
+        Dispose(false);
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            DisposeManagedResources();
+        }
+
+        DisposeUnmanagedResources();
+
+        disposed = true;
+    }
+
+    protected virtual void DisposeManagedResources()
+    {
+    }
+
+    protected virtual void DisposeUnmanagedResources()
+    {
+    }
+}
+}
